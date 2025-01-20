65b92f2481bb9469c55c37e5fa8b4471aa36437e
manethpak <manethpak00@gmail.com>
2025-01-20 16:51:56 +0700

    feat: payload-cms integration

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,20 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
+import { withPayload } from '@payloadcms/next/withPayload'
 
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "*.dreamslab.dev",
-        pathname: "",
-      }
+        protocol: 'https',
+        hostname: '*.dreamslab.dev',
+        pathname: '',
+      },
     ],
-  }
-};
+  },
+  experimental: {
+    reactCompiler: false,
+  },
+}
 
-export default nextConfig;
+export default withPayload(nextConfig)
---
+++ src/payload.config.ts
@@ -0,0 +1,29 @@
+import sharp from 'sharp'
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { postgresAdapter } from '@payloadcms/db-postgres'
+import { buildConfig } from 'payload'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { Users } from './lib/collections/Users'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    importMap: {
+      baseDir: path.resolve(dirname),
+    },
+  },
+  collections: [Users],
+  editor: lexicalEditor(),
+  secret: process.env.PAYLOAD_SECRET || '',
+  typescript: {
+    outputFile: path.resolve(dirname, 'payload-types.ts'),
+  },
+  db: postgresAdapter({
+    pool: { connectionString: process.env.DATABASE_URI },
+  }),
+  sharp,
+})
